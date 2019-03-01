0d07fabf0d6e83920c78e13c81c05ce2f5055924
S <123.com>
2019-03-01 13:33:33 +0800

    初始项目文档上传

---
+++ weixin_androidstudio/app/src/main/java/cn/buaa/myweixin/ChatActivity.java
@@ -0,0 +1,143 @@
+package cn.buaa.myweixin;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.LevelListDrawable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+
+
+public class ChatActivity extends Activity implements OnClickListener{
+    /* Called when the activity is first created. */
+
+	private Button mBtnSend;
+	private Button mBtnBack;
+	private EditText mEditTextContent;
+	private ListView mListView;
+	private ChatMsgViewAdapter mAdapter;
+	private List<ChatMsgEntity> mDataArrays = new ArrayList<ChatMsgEntity>();
+	
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_xiaohei);
+        //
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); 
+        initView();
+        
+        initData();
+    }
+    
+    
+    public void initView()
+    {
+    	mListView = (ListView) findViewById(R.id.listview);
+    	mBtnSend = (Button) findViewById(R.id.btn_send);
+    	mBtnSend.setOnClickListener(this);
+    	mBtnBack = (Button) findViewById(R.id.btn_back);
+    	mBtnBack.setOnClickListener(this);
+    	
+    	mEditTextContent = (EditText) findViewById(R.id.et_sendmessage);
+    }
+    
+    private String[]msgArray = new String[]{"男人真是口是心非", "怎么样？来打我呀！", "我舍不得", "嘻嘻嘻嘻",
+    										"脏了我的手。", "mmp",
+    										"你今天吃的什么?", "栗子鸡茄子煲双椒蒸鱼头鸡蛋羹啤酒鸭酸辣土豆丝","闭嘴...",};
+    
+    private String[]dataArray = new String[]{"2018-12-19 18:00", "2018-12-19 18:02",
+    										"2018-12-19 18:03", "2018-12-19 18:04",
+    										"2018-12-19 18:05", "2018-12-19 18:06",
+    										"2018-12-20 13:05", "2018-12-20 13:10","2018-12-20 13:11",};
+    private final static int COUNT = 9;
+    public void initData()
+    {
+    	for(int i = 0; i < COUNT; i++)
+    	{
+    		ChatMsgEntity entity = new ChatMsgEntity();
+    		entity.setDate(dataArray[i]);
+    		if (i % 2 == 0)
+    		{
+    			entity.setName("您的沙雕网友");
+    			entity.setMsgType(true);
+    		}else{
+    			entity.setName("卡哇伊的长腿欧巴");
+    			entity.setMsgType(false);
+    		}
+    		
+    		entity.setText(msgArray[i]);
+    		mDataArrays.add(entity);
+    	}
+
+    	mAdapter = new ChatMsgViewAdapter(this, mDataArrays);
+		mListView.setAdapter(mAdapter);
+		
+    }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId())
+		{
+		case R.id.btn_send:
+			send();
+			break;
+		case R.id.btn_back:
+			finish();
+			break;
+		}
+	}
+	
+	private void send()
+	{
+		String contString = mEditTextContent.getText().toString();
+		if (contString.length() > 0)
+		{
+			ChatMsgEntity entity = new ChatMsgEntity();
+			entity.setDate(getDate());
+			entity.setName("123");
+			entity.setMsgType(false);
+			entity.setText(contString);
+			
+			mDataArrays.add(entity);
+			mAdapter.notifyDataSetChanged();
+			
+			mEditTextContent.setText("");
+			
+			mListView.setSelection(mListView.getCount() - 1);
+		}
+	}
+	
+    private String getDate() {
+        Calendar c = Calendar.getInstance();
+
+        String year = String.valueOf(c.get(Calendar.YEAR));
+        String month = String.valueOf(c.get(Calendar.MONTH));
+        String day = String.valueOf(c.get(Calendar.DAY_OF_MONTH) + 1);
+        String hour = String.valueOf(c.get(Calendar.HOUR_OF_DAY));
+        String mins = String.valueOf(c.get(Calendar.MINUTE));
+        
+        
+        StringBuffer sbBuffer = new StringBuffer();
+        sbBuffer.append(year + "-" + month + "-" + day + " " + hour + ":" + mins); 
+        						
+        						
+        return sbBuffer.toString();
+    }
+    
+    
+    public void head_xiaohei(View v) {
+    	Intent intent = new Intent (ChatActivity.this,InfoXiaohei.class);			
+		startActivity(intent);	
+      } 
+}
